39417b7d120ca018a869788d2c74a71e7bfaff7f
unknown <santoshprakashsahu3@gmail.com>
2021-03-24 08:41:49 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CourseCardComponent } from './course-card/course-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseCardComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
